3ee4709ff6a5565cc7b9ffc444c8b903df878dfa
DKhorkov <dkhorkov.dev@gmail.com>
2024-04-28 14:13:27 +0300

    Added flake8 and mypy

--- src/config.py
+++ src/config.py
@@ -1,8 +1,35 @@
 from pathlib import Path
 from dataclasses import dataclass
+from pydantic_settings import BaseSettings
+from typing import List
 
 
 @dataclass(frozen=True)
-class Paths:
-    TEMPLATES: Path = Path("./templates")
-    STATIC: Path = Path("./static")
+class PathsConfig:
+    TEMPLATES: Path = Path('./templates')
+    STATIC: Path = Path('./static')
+
+    INDEX_PAGE: Path = Path('index.html')
+
+
+@dataclass(frozen=True)
+class PageNamesConfig:
+    INDEX_PAGE: str = 'Benefit Bistro'
+
+
+class CORSConfig(BaseSettings):
+    ALLOW_ORIGINS: List[str]
+    ALLOW_HEADERS: List[str]
+    ALLOW_CREDENTIALS: bool
+    ALLOW_METHODS: List[str]
+
+
+class UvicornConfig(BaseSettings):
+    HOST: str = '0.0.0.0'
+    PORT: int = 8000
+    LOG_LEVEL: str = 'info'
+    RELOAD: bool = True
+
+
+cors_config = CORSConfig()
+uvicorn_config = UvicornConfig()
