09e2d794751024b6a5a74ac9afa9cb1339d2c477
DKhorkov <dkhorkov.dev@gmail.com>
2024-04-28 14:13:27 +0300

    Added flake8 and mypy

--- src/main.py
+++ src/main.py
@@ -1,34 +1,42 @@
-import os
 import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 
-from src.config import Paths
+from src.config import PathsConfig, cors_config, PageNamesConfig, uvicorn_config
 
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory=Paths.STATIC), name="static")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=cors_config.ALLOW_ORIGINS,
+    allow_credentials=cors_config.ALLOW_CREDENTIALS,
+    allow_methods=cors_config.ALLOW_METHODS,
+    allow_headers=cors_config.ALLOW_HEADERS,
+)
 
-templates = Jinja2Templates(directory=Paths.TEMPLATES)
+app.mount('/static', StaticFiles(directory=PathsConfig.STATIC), name='static')
+
+templates = Jinja2Templates(directory=PathsConfig.TEMPLATES.__str__())
 
 
-@app.get("/", response_class=HTMLResponse)
+@app.get('/', response_class=HTMLResponse)
 async def homepage(request: Request):
     return templates.TemplateResponse(
-        name="index.html",
+        name=PathsConfig.INDEX_PAGE.__str__(),
         request=request,
-        context={"title": "Benefit Bistro"},
+        context={'title': PageNamesConfig.INDEX_PAGE},
     )
 
 
 if __name__ == '__main__':
     uvicorn.run(
-        "src.main:app",
-        host=os.getenv("HOST", default="0.0.0.0"),
-        port=os.getenv("PORT", default=8080),
-        log_level="info",
-        reload=True
+        'src.main:app',
+        host=uvicorn_config.HOST,
+        port=uvicorn_config.PORT,
+        log_level=uvicorn_config.LOG_LEVEL,
+        reload=uvicorn_config.RELOAD
     )
